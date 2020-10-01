71ae07e52000739e604c5c260ac47c51034f9c42
ya0201 <you@example.com>
2020-10-01 21:52:23 +0900

    format

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-        "net/http"
+	"net/http"
 
-        "github.com/prometheus/client_golang/prometheus"
-        "github.com/prometheus/client_golang/prometheus/promauto"
-        "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-        gib3700 = promauto.NewGaugeVec(prometheus.GaugeOpts{
-                Name: "gibibytes_3700",
-                Help: "This value represents 3700 * 1024 * 1024 * 1024 bytes",
-        }, []string{"label1"})
+	gib3700 = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "gibibytes_3700",
+		Help: "This value represents 3700 * 1024 * 1024 * 1024 bytes",
+	}, []string{"label1"})
 )
 
 func main() {
-        gib3700.WithLabelValues("please-print-me-as-3700gib").Set(3700)
+	gib3700.WithLabelValues("please-print-me-as-3700gib").Set(3700)
 
-        http.Handle("/metrics", promhttp.Handler())
-        http.ListenAndServe(":8080", nil)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
 }
