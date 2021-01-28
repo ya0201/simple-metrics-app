208553e936346890e31a79cc3faf5412242ccefd
ya0201 <you@example.com>
2021-01-28 17:59:05 +0900

    Add health endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,9 +16,14 @@ var (
 	}, []string{"label1"})
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "200 ok")
+}
+
 func main() {
 	gib3700.WithLabelValues("please-print-me-as-3700gib").Set(3700)
 
+	http.HandleFunc("/health", healthHandler)
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
